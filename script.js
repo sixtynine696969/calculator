543586c1f369ed192aa2b374315b47c4a52934f4
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-06-12 02:51:24 +0200

    Write operate function

--- script.js
+++ script.js
@@ -11,5 +11,30 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 - num2;
+    return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+    num1 = +num1;
+    num2 = +num2;
+
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case 'x':
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+let num = '3 / 5';
+let arr = num.split(' ');
+let num1 = arr[0];
+let operator = arr[1];
+let num2 = arr[2];
+
+console.log(operate(operator, num1, num2));
